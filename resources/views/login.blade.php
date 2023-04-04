e31c0cc4653c630067b626fbdd00b674a6a495fd
Aravinthansparrow <aravinthan.istardevelopers@gmail.com>
2023-04-04 23:38:01 +0530

    css login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,15 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 </head>
 <body>
-    <form action="show" >
-        <label for="registernumber">Register Number</label>
-        <input type="text" name="registernumber"><br>
-        <label for="password">Password</label>
-        <input type="text" name="password"><br>
-        <input type="submit">
-    </form>
+    <section class="form-section">
+        <form action="show" class="login-grp">
+            <div class="field-grp">
+                <label class="head-title" for="registernumber">Register Number</label>
+                <input class="input-field" type="text" name="registernumber">
+            </div>
+            <div class="field-grp">
+                <label class="head-title" for="password">Password</label>
+                <input class="input-field" type="text" name="password"><br>
+            </div>
+            <input type="submit" class="submit-field">
+        </form>
+    </section>
     
 </body>
 </html>
